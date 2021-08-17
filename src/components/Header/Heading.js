3d189f9210530509709a19f04a889e0098fd11be
Lache Porter <73504082+lacheporter@users.noreply.github.com>
2021-08-17 16:19:13 -0400

    changed color

--- src/components/Header/Heading.js
+++ src/components/Header/Heading.js
@@ -1,12 +1,12 @@
 import React from "react";
-import GFEBS from "../../assets/WhiteGFEBS.svg";
+import logo from "../../logo.svg";
 import styles from "../../Styles/main.module.css";
 
 export default function Heading({ title, subtitle }) {
   return (
     <div className={styles.headingContainer}>
-      <img src={GFEBS} className={styles.headingLogo} />
-      <div className={styles.headingDivider} />
+      <img src={logo} className={styles.headingLogo} />
+      {/* <div className={styles.headingDivider} /> */}
       <div>
         <h3 className={styles.headingTitle}>{title}</h3>
         <h3 className={styles.headingSubtitle}>{subtitle}</h3>
