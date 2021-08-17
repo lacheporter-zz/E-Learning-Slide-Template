9e3b902a24bceda10fba92891f71f981cf3fdcf7
Lache Porter <73504082+lacheporter@users.noreply.github.com>
2021-08-17 15:59:59 -0400

    changed slidedata

--- src/data/SlideData.js
+++ src/data/SlideData.js
@@ -1,30 +1,22 @@
-import HRPayroll from "../assets/hr_payroll.svg";
-import LOA from "../assets/LOA.svg";
+import placholderImage from "../assets/placeholder_image.svg";
 import checkpoint from "../assets/checkpoint.svg";
 import objectives from "../assets/objectives.svg";
-// import HRPA from "../assets/HRPA.svg";
-import HumanResource from "../assets/human_resource.svg";
-import DefenseCivilian from "../assets/defense_civilian.svg";
-import ArmyCivilian1 from "../assets/ArmyCivilian1.svg";
-import ArmyCivilian2 from "../assets/ArmyCivilian2.svg";
-import ArmyCivilian3 from "../assets/ArmyCivilian3.svg";
-
-const imageSize = "100%";
 
 // function formatted() {
 //   return <span style={{ fontWeight: "bold" }}>Training</span>;
 // }
 // var test = formatted();
+const imageSize = "100%";
 
 const SlideData = [
   {
     index: 0,
-    title: "Welcome to Overview of the GFEBS HR & Payroll/ Labor Process",
+    title: "Slide Title 1",
     layout: "standard",
     content: [
       {
         type: "paragraph",
-        text: "Welcome to the Overview of the GFEBS HR & Payroll/ Labor Process module. In this module we will discuss how the Human Resource Module interfaces with the Defense Civilian Personnel System (DCPDS), and the Army Civilian Personnel System (ACPERS).Welcome to the General Fund Enterprise Business System (GFEBS) Human Resource (HR) and Payroll/ Labor Processes lesson. In this introduction, an overview of important concepts, terminology, and roles are explained.",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
       },
       {
         type: "space",
@@ -32,39 +24,34 @@ const SlideData = [
       },
       {
         type: "paragraph",
-        text: "Upon completion of this module you should be able to identify these systems, important components that comprise them, and how they interface with GFEBS.",
+        text: "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
       },
     ],
-    image: HRPayroll,
+    image: placholderImage,
     width: imageSize,
   },
   {
     index: 1,
-    title: "Objectives",
+    title: "Slide Title 2",
     layout: "standard",
     content: [
       {
         type: "list",
-        title: "At the end of this module, you should be able to:",
-        items: [
-          "Identify personnel systems that interface with GFEBS",
-          "Recognize the components & concepts of systems used for organizational management",
-          "Define the processes of how GFEBS receives data from personnel systems",
-          "Identify specific data exchanged between GFEBS and interfacing systems",
-        ],
+        title: "List Header",
+        items: ["List item 1", "List item 2", "List item 3", "List item 4"],
       },
     ],
-    image: objectives,
+    image: placholderImage,
     width: imageSize,
   },
   {
     index: 2,
-    title: "Module Lead-In (1 of 3",
+    title: "Slide Title 3",
     layout: "standard",
     content: [
       {
         type: "paragraph",
-        text: "The Human Resource (HR) Module of GFEBS has several components or sub-modules for organization management and for processing personnel actions and payroll.",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
       },
 
       {
@@ -73,7 +60,7 @@ const SlideData = [
       },
       {
         type: "paragraph",
-        text: "The GFEBS’ limited scope will only use the Human Resource Personnel Administration (HRPA) component.",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
       },
       {
         type: "space",
@@ -81,7 +68,7 @@ const SlideData = [
       },
       {
         type: "paragraph",
-        text: "HRPA will only house employee master data necessary to facilitate the pay, labor process, and reporting.",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
       },
       {
         type: "space",
@@ -89,25 +76,21 @@ const SlideData = [
       },
       {
         type: "list",
-        title: "Examples are: ",
-        items: [
-          "Employee Line of Accounting (LOA)",
-          "Employee personnel information used for manpower reporting",
-          "Employee identifying information (e.g., UIC, Cost Center)",
-        ],
+        title: "List Heading:",
+        items: ["List item 1", "List item 2", "List item 3", "List item 4"],
       },
     ],
-    image: objectives,
+    image: placholderImage,
     width: imageSize,
   },
   {
     index: 3,
-    title: "Module Lead-In (2 of 3)",
+    title: "Slide Title 4",
     layout: "standard",
     content: [
       {
         type: "paragraph",
-        text: "GFEBS is the system of record for the Line of Accounting (LOA) used for posting to DCPS, ATAAPS and the Manual Time-Tracking upload spreadsheet. Once an end user has added the LOA to an employee record, GFEBS sends the basic treasury information associated with the GFEBS FUND to DCPS to update the employees accounting classification. ",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
       },
       {
         type: "space",
@@ -117,22 +100,22 @@ const SlideData = [
         type: "list",
         title: "",
         items: [
-          "Employee records must be updated by end users with LOA before the pay and labor files can process. Missing records will cause these transactions to fail.",
-          "Users can maintain the employee LOA through manual entry or spreadsheet upload.",
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
         ],
       },
     ],
-    image: LOA,
+    image: placholderImage,
     width: imageSize,
   },
   {
     index: 4,
-    title: "Module Lead-In (3 of 3)",
+    title: "Slide Title 5",
     layout: "wide",
     content: [
       {
         type: "paragraph",
-        text: "Examples of the basic treasury information that GFEBS sends DCPS are as follows:",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua: ",
       },
       {
         type: "space",
@@ -142,20 +125,20 @@ const SlideData = [
         type: "terms",
         terms: [
           {
-            name: "Department Code",
-            definition: "Example: 21",
+            name: "Term 1",
+            definition: "Definition 1",
           },
           {
-            name: "Appropriation and Limitation",
-            definition: "Example: 2020.0012",
+            name: "Term 2",
+            definition: "Definition 2",
           },
           {
-            name: "Fiscal Year",
-            definition: "Example: FYXX",
+            name: "Term 3",
+            definition: "Definition 3",
           },
           {
-            name: "Fiscal Station Number",
-            definition: "Example: 80420",
+            name: "Term 4",
+            definition: "Definition 4",
           },
         ],
       },
@@ -163,130 +146,34 @@ const SlideData = [
   },
   {
     index: 5,
-    title: "The Human Resource Module",
+    title: "Slide Title 6",
     layout: "standard",
     content: [
       {
         type: "paragraph",
-        text: "The Human Resource Module in GFEBS has several components used for organizational management, processing personnel actions and payroll. The Human Resource (HR) Module of GFEBS stores related employee data in individual tables. Users of GFEBS can create, change and view employee data via 'Infotypes'. Infotypes are employee master data grouped together in HR according to subject matter and logical groups.",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
       },
     ],
-    image: HumanResource,
+    image: placholderImage,
     width: imageSize,
   },
   {
     index: 6,
-    title: "The Defense Civilian Personnel Data System (DCPDS)",
+    title: "Slide Title 7",
     layout: "standard",
     content: [
       {
         type: "paragraph",
 
-        text: "The Defense Civilian Personnel Data System (DCPDS) is a civilian human resources information support system for the Department of Defense (DOD). The system is designed to support appropriated fund, nonappropriated fund, and local national human resources operations. DCPDS is the system of record for all DOD civilian employee master data. DCPDS sends employee master records to the Defense Civilian Pay System (DCPS) for payroll processing and to the Army Civilian Personnel System (ACPERS) for employee master data creation and updates. ACPERS is the Army’s civilian management information system that stores Army civilian employee master data and serves as a hub between DCPDS and other partner applications for manpower management and reporting",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
       },
     ],
-    image: DefenseCivilian,
+    image: placholderImage,
     width: imageSize,
   },
+
   {
     index: 7,
-    title: "Army Civilian Personnel System (ACPERS) (1 of 3)",
-    layout: "standard",
-    content: [
-      {
-        type: "paragraph",
-
-        text: "The inbound ACPERS interface creates and updates personnel records through various personnel actions (Hiring, Leaving, Rehire, Deactivate and Reactivate) providing data to meet Manpower reporting requirements. Manpower reporting enables the Army to understand the composition of its workforce and assist in decisions on workforce staffing and funding decisions. In GFEBS, the execution of the above processes modifies the employment status of an employee, populates required fields and delimits (end date) of the LOA Infotype. Infotypes are the screen presentation view of the stored data on the corresponding tables.",
-      },
-    ],
-    image: ArmyCivilian1,
-    width: imageSize,
-  },
-  {
-    index: 8,
-    title: "Army Civilian Personnel System (ACPERS) (2 of 3)",
-    layout: "standard",
-    content: [
-      {
-        type: "paragraph",
-
-        text: "GFEBS receives a daily file from ACPERS containing employee master data for creating and updating employee records in the GFEBS Human Resources (HR) module. The employee SSN is the key identifier between ACPERS and GFEBS. The LOA is created manually or via the upload spreadsheet and not through the ACPERS interface. GFEBS uses the employee record for several purposes to include:",
-      },
-      {
-        type: "space",
-        height: "1em",
-      },
-      {
-        type: "paragraph",
-        text: "• Capturing the accounting information necessary to process pay and labor transactions. The Line of Accounting (LOA) created for each employee in HR is used for:",
-      },
-      {
-        type: "list",
-        title: "",
-        items: [
-          "Posting payroll from the Defense Civilian Pay System (DCPS) to the HR LOA.",
-          "Allocating labor from the HR LOA to other cost objects charged.",
-          "Sending Treasury data to DCPS daily which DCPS uses to disburse payroll.",
-        ],
-      },
-      {
-        type: "space",
-        height: "1em",
-      },
-      {
-        type: "paragraph",
-        text: "• Housing employee attributes (e.g., Civilian Type, Temp Position) for manpower reporting.",
-        size: "1.2rem",
-      },
-      {
-        type: "space",
-        height: "1em",
-      },
-      {
-        type: "paragraph",
-        text: "• Associating employee attributes with employee level transactions in BI reports. HR employee records are extracted to BI and can be displayed in most labor and pay reports.",
-        size: "1.2rem",
-      },
-    ],
-    image: ArmyCivilian2,
-    width: imageSize,
-  },
-
-  {
-    index: 9,
-    title: "Army Civilian Personnel System (ACPERS) (3 of 3)",
-    layout: "standard",
-    content: [
-      {
-        type: "paragraph",
-        text: "ACPERS is designed to only send data for employees designated as Army (Agency Code = ‘AR’). Those employees must be Appropriated Fund Army (Employee Type = ‘1’).",
-        size: "1.2rem",
-      },
-      {
-        type: "space",
-        height: "1em",
-      },
-      {
-        type: "paragraph",
-        text: "This table displays the details of the creation of Employee Groups.",
-        size: "1.2rem",
-      },
-      {
-        type: "space",
-        height: "1em",
-      },
-      {
-        type: "paragraph",
-        text: "GFEBS HR reports are available in ECC and BI to allow users to search by multiple parameters. These reports display employee records and associated LOA. The reports also include the ability to search for records where the LOA is null, indicating the new employee was added (manually or interfaced) but is missing the LOA.",
-        size: "1.2rem",
-      },
-    ],
-    image: ArmyCivilian3,
-    width: "100%",
-  },
-
-  {
-    index: 10,
     title: "Lesson Checkpoint",
     content: [
       {
